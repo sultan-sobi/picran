cf1f37ac823bf59a3b1e7e9cd823bc0a9f4da682
Badsha <108751620+sultan-sobi@users.noreply.github.com>
2025-01-12 20:34:52 +0600

    html css js are added

---
+++ script.js
@@ -0,0 +1,20 @@
+// const btnE= document.querySelector(".btn");
+// btnE.addEventListener("mouseover",(event)=>{
+//   const x= (event.pageX-btnE.offsetLeft);
+//   const y= (event.pagey-btnE.offsetTop);
+
+//   btnE.style.setProperty("--xPos", x+"px");
+//   btnE.style.setProperty("--yPos", y+"px");
+  
+// })
+
+
+// const btnE=document.querySelector(".btn");
+
+// btnE.addEventListener("event",(event)=>{
+//   const x= (event.pageX-btnE.offsetLeft);
+//   const y= (event.pageY-btnE.offsetTop);
+
+//   btnE.style.setProperty("--xPos", x+"px");
+//   btnE.style.setProperty("--yPos", y+"px");
+// })
